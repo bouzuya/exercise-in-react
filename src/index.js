7954adea2cb72f5052587d25876e30f72d583f29
bouzuya <m@bouzuya.net>
2015-05-02 19:25:09 +0900

    add simple component

---
+++ src/index.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var HelloMessage = React.createClass({
+  displayName: 'HelloMessage',
+  render: function() {
+    return React.createElement('div', null, 'Hello ', this.props.name);
+  }
+});
+
+React.render(
+  React.createElement(HelloMessage, { name: 'John' }),
+  document.getElementById('container')
+);
