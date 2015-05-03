d7d7131dba101b1717da2788eaba959e2e26899c
bouzuya <m@bouzuya.net>
2015-05-03 13:11:21 +0900

    add ex2 (Timer)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
+// var ex1 = require('./ex1');
 var React = require('react');
 
-var HelloMessage = React.createClass({
-  displayName: 'HelloMessage',
+var Timer = React.createClass({
+  displayName: 'Timer',
+  getInitialState: function() {
+    return { secondsElapsed: 0 };
+  },
+  tick: function() {
+    this.setState({ secondsElapsed: this.state.secondsElapsed + 1 });
+  },
+  componentDidMount: function() {
+    this.interval = setInterval(this.tick, 1000);
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
   render: function() {
-    return React.createElement('div', null, 'Hello ', this.props.name);
+    return React.createElement(
+      'div', null, 'Seconds Elapsed: ', this.state.secondsElapsed);
   }
 });
 
 React.render(
-  React.createElement(HelloMessage, { name: 'John' }),
+  React.createElement(Timer),
   document.getElementById('container')
 );
