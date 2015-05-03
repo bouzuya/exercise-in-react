6ec0db79a8dad7ae80cfc674167fb5d851cd59d3
bouzuya <m@bouzuya.net>
2015-05-03 13:45:09 +0900

    check event timing

--- src/index.js
+++ src/index.js
@@ -5,18 +5,20 @@ var React = require('react');
 var Hello2 = React.createClass({
   displayName: 'Hello2',
   getInitialState: function() {
-    return { message: ''};
+    return { message: 'bouzuya'};
   },
   onTextChanged: function(e) {
+    console.log('changed!');
     this.setState({ message: e.target.value });
   },
   render: function() {
+    console.log('render!');
     return React.createElement(
       'div', null,
       React.createElement(
         'form', null,
         React.createElement(
-          'input', { onChange: this.onTextChanged }
+          'input', { onChange: this.onTextChanged, value: this.state.message }
         )
       ),
       React.createElement('div', null, 'Hello, ', this.state.message)
