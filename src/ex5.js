7e423caba9b1e7930eb9c00b7b51c2af181ccdf3
bouzuya <m@bouzuya.net>
2015-05-03 14:53:27 +0900

    some messages

---
+++ src/ex5.js
@@ -0,0 +1,64 @@
+var React = require('react');
+
+var MessageInput = React.createClass({
+  displayName: 'MessageInput',
+  onChange: function(e) {
+    this.props.onChange(e.target.value);
+  },
+  onKeyDown: function(e) {
+    if (e.keyCode === 13) {
+      this.props.onSubmit(e.target.value);
+      e.target.value = '';
+    }
+  },
+  render: function() {
+    return React.createElement('input', {
+      onChange: this.onChange, onKeyDown: this.onKeyDown
+    });
+  }
+});
+
+var MessageOutput = React.createClass({
+  displayName: 'MessageOutput',
+  render: function() {
+    return React.createElement(
+      'div', null,
+      React.createElement('span', null, 'Hello, ', this.props.message),
+      React.createElement(
+        'ul', null,
+        this.props.messages.map(function (i, index) {
+          return React.createElement('li', { key: index }, i);
+        })
+      )
+    );
+  }
+});
+
+var Hello3 = React.createClass({
+  displayName: 'Hello3',
+  getInitialState: function() {
+    return { message: 'bouzuya', messages: [] };
+  },
+  onTextChanged: function(text) {
+    this.setState({ message: text });
+  },
+  onTextSubmitted: function(text) {
+    var messages = this.state.messages.concat(text);
+    this.setState({ messages: messages });
+  },
+  render: function() {
+    return React.createElement(
+      'div', null,
+      React.createElement(MessageInput, {
+        onChange: this.onTextChanged,
+        onSubmit: this.onTextSubmitted
+      }),
+      React.createElement(MessageOutput, {
+        message: this.state.message,
+        messages: this.state.messages
+      })
+    );
+  }
+});
+
+module.exports = React.createElement(Hello3);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ var React = require('react');
 // var ex1 = require('./ex1');
 // var ex2 = require('./ex2');
 // var ex3 = require('./ex3');
-var ex4 = require('./ex4');
+// var ex4 = require('./ex4');
+var ex = require('./ex5');
 
-React.render(ex4, document.getElementById('container'));
+React.render(ex, document.getElementById('container'));
